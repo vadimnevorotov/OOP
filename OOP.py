9d1f9cb1c0ab22b5da94c7fefa824fce4f572f10
Вадим Неворотов <vadimtilda@gmail.com>
2022-02-20 22:56:41 +0500

    third task

--- OOP.py
+++ OOP.py
@@ -17,6 +17,16 @@ class Student:
         else:
             return 'Ошибка'
 
+    def __str__(self):
+        res = f'Имя: {name} + \n Фамилия: {surname} + \n Средняя оценка за домашние задания: {sum(self.grades.values())/len(self.grades)} +\n курсы..+\n'
+        return res
+
+    def __lt__(self, student):
+        if not isinstance(student, Student):
+            print(f'student is not a valid student')
+        else:
+            return sum(self.grades.values()) / len(self.grades) < sum(student.grades.values()) / len(student.grades)
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -30,10 +40,24 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
 
+    def __str__(self):
+        res = f'Имя: {name} +\n + Фамилия: {surname} + \n + Средняя оценка за лекции: {sum(self.grades.values()) / len(self.grades)}'
+        return res
+
+    def __lt__(self, other):
+        if not isinstance(other, Lecturer):
+            print(f'{other} is not a lecturer')
+            return
+        else:
+            return sum(self.grades.values())/len(self.grades.values()) < sum(other.grades.values())/len(other.grades.values())
+
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
+
+    def __str__(self):
+        return print(f'Имя: {name} \n Фамилия:{surname}')
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
